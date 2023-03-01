517bd0fdc2ef5308ae66bbcb959d346b1019cef7
Vitalii Boiko <vitalii.boiko@playtomax.com>
2023-03-01 13:35:59 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Contact from "./components/Contact/Contact";
+import Hero from "./components/Hero/Hero";
+import Who from "./components/Who/Who";
+import Works from "./components/Works/Works";
 
-function App() {
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
   );
 }
-
-export default App;
---
+++ src/components/Who/Who.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Who() {
+  return <div>Who</div>;
+}
