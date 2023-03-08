c336a9e069350b93daf7a16f3218767a3938d360
Vitalii Boiko <vitalii.boiko@playtomax.com>
2023-03-08 15:40:59 +0200

    add form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import styled from "styled-components";
 
 const Section = styled.div`
@@ -6,6 +7,100 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: lightcoral;
+  border: none;
+  border-radius: 5px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  background-color: lightcoral;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 export default function Contact() {
-  return <Section>Contact</Section>;
+  const ref = useRef();
+  const [succsess, setSuccess] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_5q5834p",
+        "template_d7btl4o",
+        ref.current,
+        "RzJYtCKQ872GwWDt7"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess(false);
+        }
+      );
+  };
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Form ref={ref} onSubmit={handleSubmit}>
+            <Title>Contact Me</Title>
+            <Input placeholder="Name" name="name" />
+            <Input placeholder="Email" name="email" />
+            <TextArea placeholder="Message" name="message" rows={10} />
+            <Button type="submit">Send</Button>
+            {succsess && `Message send`}
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 }
--- src/components/Who/Who.jsx
+++ src/components/Who/Who.jsx
@@ -4,8 +4,75 @@ import styled from "styled-components";
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+const Title = styled.h1`
+  font-size: 74px;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 5px;
+`;
+const SubTitle = styled.h2`
+  color: green;
+`;
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgrey;
+`;
+const Button = styled.button`
+  background-color: red;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 export default function Who() {
-  return <Section>Who</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left></Left>
+        <Right>
+          <Title>Work as web!</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <SubTitle>Work as web!</SubTitle>
+          </WhatWeDo>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iste
+            numquam minima dolore reiciendis sed.
+          </Desc>
+          <Button>Learn More</Button>
+          {/* <Img src="./img/Earth.jpg" /> */}
+        </Right>
+      </Container>
+    </Section>
+  );
 }
